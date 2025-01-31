5007327af63a318348574d5b74ca02fd338e67ad
Jamin Eisenberg <109154023+jeisenberg-chwy@users.noreply.github.com>
2025-01-31 07:27:47 -0500

    Include assoc set dep

--- src/Evergreen/V4/Types.elm
+++ src/Evergreen/V4/Types.elm
@@ -1,8 +1,32 @@
 module Evergreen.V4.Types exposing (..)
 
+import AssocSet
+import Pixels
+import Quantity
+
+
+type Key
+    = K_Up
+    | K_Right
+    | K_Down
+    | K_Left
+    | K_W
+    | K_A
+    | K_S
+    | K_D
+
+
+type alias Keys =
+    AssocSet.Set Key
+
 
 type alias FrontendModel =
-    {}
+    { keysPressed : Keys
+    , viewportSize :
+        { width : Quantity.Quantity Int Pixels.Pixels
+        , height : Quantity.Quantity Int Pixels.Pixels
+        }
+    }
 
 
 type alias BackendModel =
@@ -11,10 +35,18 @@ type alias BackendModel =
 
 type FrontendMsg
     = NoOpFrontendMsg
+    | KeyDown Key
+    | KeyUp Key
+    | Tick Float
+    | Resize
+        { width : Int
+        , height : Int
+        }
 
 
 type ToBackend
     = NoOpToBackend
+    | PlayerInput Keys
 
 
 type BackendMsg
